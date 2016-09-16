b6729979d415c054bd99b5224132075965b2abf2
Andrew Klein <andrew.klein.j@gmail.com>
2016-09-16 11:45:24 -0400

    Added Console validations to model

--- app/models/console.rb
+++ app/models/console.rb
@@ -1,4 +1,5 @@
 class Console < ApplicationRecord
   has_many :consoles_video_games
   has_many :video_games, through: :consoles_video_games
+  validates :name, :manufacturer, :release_date, presence: true
 end
