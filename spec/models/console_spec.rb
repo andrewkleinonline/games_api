8d151c35d523851574ffb69376d1784e406596c5
Andrew Klein <andrew.klein.j@gmail.com>
2016-09-16 11:26:23 -0400

    add test for validity of console

--- spec/models/console_spec.rb
+++ spec/models/console_spec.rb
@@ -1,4 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Console, type: :model do
+  it 'is valid with a name, release date, and manufacturer' do
+    ps4 = Console.create(name: "PS4", release_date: Date.strptime('2013-11-15', '%Y-%m-%d'), manufacturer: "Sony")
+    expect(ps4).to be_valid
+  end
 end
