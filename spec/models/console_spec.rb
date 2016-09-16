c42725d161168eb7954ec5367a44c787038749fa
Andrew Klein <andrew.klein.j@gmail.com>
2016-09-16 11:40:40 -0400

    Added error tests for rspec model validations

--- spec/models/console_spec.rb
+++ spec/models/console_spec.rb
@@ -9,16 +9,19 @@ RSpec.describe Console, type: :model do
   #will want to refactor these tests to use a specific error rather than to_not be_valid
   it 'is not valid without a name' do
     unnamed_console = Console.create(release_date: Date.strptime('2013-11-15', '%Y-%m-%d'), manufacturer: "Sony")
-    expect(unnamed_console).to_not be_valid
+    unnamed_console.valid?
+    expect(unnamed_console.errors[:name]).to include("can't be blank")
   end
 
   it 'is not valid without a date' do
     ps4_outside_of_spacetime = Console.create(name: "PS4", manufacturer: "Sony")
-    expect(ps4_outside_of_spacetime).to_not be_valid
+    ps4_outside_of_spacetime.valid?
+    expect(ps4_outside_of_spacetime.errors[:release_date]).to include("can't be blank")
   end
 
   it 'is not valid without a manufacturer' do
     ps4_not_made_by_anyone = Console.create(name: "PS4", release_date: Date.strptime('2013-11-15', '%Y-%m-%d'))
-    expect(ps4_not_made_by_anyone).to_not be_valid
+    ps4_not_made_by_anyone.valid?
+    expect(ps4_not_made_by_anyone.errors[:manufacturer]).to include("can't be blank")
   end
 end
