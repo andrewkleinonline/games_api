79c76240422e800cc222e5930095b51666d56b46
Andrew Klein <andrew.klein.j@gmail.com>
2016-09-16 13:21:47 -0400

    reword model specs

--- spec/models/console_spec.rb
+++ spec/models/console_spec.rb
@@ -1,27 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe Console, type: :model do
-  it 'is valid with a name, release date, and manufacturer' do
-    ps4 = Console.create(name: "PS4", release_date: Date.strptime('2013-11-15', '%Y-%m-%d'), manufacturer: "Sony")
-    expect(ps4).to be_valid
-  end
+  describe 'validity:' do
+    it 'is valid with a name, release date, and manufacturer' do
+      ps4 = Console.create(name: "PS4", release_date: Date.strptime('2013-11-15', '%Y-%m-%d'), manufacturer: "Sony")
+      expect(ps4).to be_valid
+    end
 
-  #will want to refactor these tests to use a specific error rather than to_not be_valid
-  it 'is not valid without a name' do
-    unnamed_console = Console.create(release_date: Date.strptime('2013-11-15', '%Y-%m-%d'), manufacturer: "Sony")
-    unnamed_console.valid?
-    expect(unnamed_console.errors[:name]).to include("can't be blank")
-  end
+    #will want to refactor these tests to use a specific error rather than to_not be_valid
+    it 'is not valid without a name' do
+      unnamed_console = Console.create(release_date: Date.strptime('2013-11-15', '%Y-%m-%d'), manufacturer: "Sony")
+      unnamed_console.valid?
+      expect(unnamed_console.errors[:name]).to include("can't be blank")
+    end
 
-  it 'is not valid without a date' do
-    ps4_outside_of_spacetime = Console.create(name: "PS4", manufacturer: "Sony")
-    ps4_outside_of_spacetime.valid?
-    expect(ps4_outside_of_spacetime.errors[:release_date]).to include("can't be blank")
-  end
+    it 'is not valid without a date' do
+      ps4_outside_of_spacetime = Console.create(name: "PS4", manufacturer: "Sony")
+      ps4_outside_of_spacetime.valid?
+      expect(ps4_outside_of_spacetime.errors[:release_date]).to include("can't be blank")
+    end
 
-  it 'is not valid without a manufacturer' do
-    ps4_not_made_by_anyone = Console.create(name: "PS4", release_date: Date.strptime('2013-11-15', '%Y-%m-%d'))
-    ps4_not_made_by_anyone.valid?
-    expect(ps4_not_made_by_anyone.errors[:manufacturer]).to include("can't be blank")
+    it 'is not valid without a manufacturer' do
+      ps4_not_made_by_anyone = Console.create(name: "PS4", release_date: Date.strptime('2013-11-15', '%Y-%m-%d'))
+      ps4_not_made_by_anyone.valid?
+      expect(ps4_not_made_by_anyone.errors[:manufacturer]).to include("can't be blank")
+    end
   end
 end
--- spec/models/video_game_spec.rb
+++ spec/models/video_game_spec.rb
@@ -1,30 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe VideoGame, type: :model do
-  describe "is not valid without column information" do
-    it 'is not valid when missing title' do
+  describe "validity:" do
+
+    it "is valid when contains title, release date, devloper, and publisher" do
+      game = VideoGame.new(title: 'Final Fantasy', release_date: Date.strptime('2013-11-15', '%Y-%m-%d'), developer: 'SquareEnix', publisher: 'Sony')
+      expect(game).to be_valid
+    end
+
+    it 'is not valid without a title' do
       game = VideoGame.new(release_date: Date.strptime('2013-11-15', '%Y-%m-%d') , developer: 'SquareEnix', publisher: 'Sony')
       game.valid?
       expect(game.errors[:title]).to include("can't be blank")
     end
 
-    it 'is not valid when missing release date' do
+    it 'is not valid without a release date' do
       game = VideoGame.new(title: 'Final Fantasy', developer: 'SquareEnix', publisher: 'Sony')
       game.valid?
       expect(game.errors[:release_date]).to include("can't be blank")
     end
 
-    it 'is not valid when missing developer' do
+    it 'is not valid without a developer' do
       game = VideoGame.new(title: 'Final Fantasy', release_date: Date.strptime('2013-11-15', '%Y-%m-%d'), publisher: 'Sony')
       game.valid?
       expect(game.errors[:developer]).to include("can't be blank")
     end
 
-    it "is valid when contains all required column information" do
-      game = VideoGame.new(title: 'Final Fantasy', release_date: Date.strptime('2013-11-15', '%Y-%m-%d'), developer: 'SquareEnix')
-      expect(game).to be_valid
+    it 'is not valid without a publisher' do
+      game = VideoGame.new(title: 'Final Fantasy', release_date: Date.strptime('2013-11-15', '%Y-%m-%d'), developer: 'SquareEnix', publisher: 'Sony')
+      game.valid?
+      expect(game.errors[:publisher]).to include("can't be blank")
     end
-
   end
 
 end
